8aa8c5c3cd0f35019463b353d0c460ea3f734b00
ChinHui-Chen <dongogo@gmail.com>
2018-04-08 01:56:04 +0800

    preprocessing scoring

--- bundles/aq_competition_bundle/scoring_program/score.py
+++ bundles/aq_competition_bundle/scoring_program/score.py
@@ -20,13 +20,15 @@ from sys import argv
 import libscores
 import yaml
 from libscores import ls, filesep, mkdir, read_array, compute_all_scores, write_scores
+import pandas as pd
+import numpy as np
 
 # Default I/O directories:
-root_dir = "/Users/isabelleguyon/Documents/Projects/ParisSaclay/Projects/ChaLab/Examples/iris/"
-default_input_dir = root_dir + "scoring_input_1_2"
+root_dir = "./"
+default_input_dir = root_dir + "scoring_input_1_1"
 default_output_dir = root_dir + "scoring_output"
 
-# Debug flag 0: no debug, 1: show all scores, 2: also show version amd listing of dir
+# Debug flag 0: no debug, 1: show all scores, 2: also show version and listing of dir
 debug_mode = 1
 
 # Constant used for a missing score
@@ -50,7 +52,6 @@ def _load_scoring_function():
 # =============================== MAIN ========================================
 
 if __name__ == "__main__":
-
     #### INPUT/OUTPUT: Get input and output directory names
     if len(argv) == 1:  # Use the default input and output directories if no arguments are provided
         input_dir = default_input_dir
@@ -67,7 +68,7 @@ if __name__ == "__main__":
     metric_name, scoring_function = _load_scoring_function()
 
     # Get all the solution files from the solution directory
-    solution_names = sorted(ls(os.path.join(input_dir, 'ref', '*.solution')))
+    solution_names = sorted(ls(os.path.join(input_dir, 'ref', '*.csv')))
 
     # Loop over files in solution directory and search for predictions with extension .predict having the same basename
     for i, solution_file in enumerate(solution_names):
@@ -78,16 +79,26 @@ if __name__ == "__main__":
         basename = solution_file[-solution_file[::-1].index(filesep):-solution_file[::-1].index('.') - 1]
 
         try:
-            # Get the last prediction from the res subdirectory (must end with '.predict')
-            predict_file = ls(os.path.join(input_dir, 'res', basename + '*.predict'))[-1]
+            # Get the last prediction from the res subdirectory (must end with '.csv')
+            predict_file = ls(os.path.join(input_dir, 'res', '*.csv'))[-1]
             if (predict_file == []): raise IOError('Missing prediction file {}'.format(basename))
             predict_name = predict_file[-predict_file[::-1].index(filesep):-predict_file[::-1].index('.') - 1]
 
-            # Read the solution and prediction values into numpy arrays
-            solution = read_array(solution_file)
-            prediction = read_array(predict_file)
-            if (solution.shape != prediction.shape): raise ValueError(
+            # Get solution and prediction files here
+            pd_solution = pd.read_csv(solution_file, sep=',')
+            pd_prediction = pd.read_csv(predict_file, sep=',')
+            
+            # Check shape
+            if (len(pd_solution) != len(pd_prediction)): raise ValueError(
                 "Bad prediction shape {}".format(prediction.shape))
+
+            # leftjoin sol and pred data
+            pd_merged_all = pd.merge( pd_solution, pd_prediction, how='left', on=['submit_date','test_id'] )
+            # pd_merged = ['submit_date', 'test_id', 'PM2.5_x', 'PM10_x', 'O3_x', 'PM2.5_y', 'PM10_y', 'O3_y']
+            # filter missing value
+            pd_merged_pm25 = pd_merged_all[['submit_date', 'test_id', 'PM2.5_x', 'PM2.5_y']].replace('', np.nan, regex=True).dropna(subset=['PM2.5_x']).replace(np.nan, 0, regex=True)
+            pd_merged_pm10 = pd_merged_all[['submit_date', 'test_id', 'PM10_x', 'PM10_y']].replace('', np.nan, regex=True).dropna(subset=['PM10_x']).replace(np.nan, 0, regex=True)
+            pd_merged_o3 = pd_merged_all[['submit_date', 'test_id', 'O3_x', 'O3_y']].replace('', np.nan, regex=True).dropna(subset=['O3_x']).replace(np.nan, 0, regex=True)
 
             try:
                 # Compute the score prescribed by the metric file 
@@ -116,14 +127,6 @@ if __name__ == "__main__":
         score_file.write(score_name + ": %0.12f\n" % score)
 
     # End loop for solution_file in solution_names
-
-    # Read the execution time and add it to the scores:
-    try:
-        metadata = yaml.load(open(os.path.join(input_dir, 'res', 'metadata'), 'r'))
-        score_file.write("Duration: %0.6f\n" % metadata['elapsedTime'])
-    except:
-        score_file.write("Duration: 0\n")
-
         html_file.close()
     score_file.close()
 
